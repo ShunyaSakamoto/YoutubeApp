9144ec2c585c11e4a961699ffb9ca7db46a2df33
Shunya Sakamoto <syunya27212@gmail.com>
2023-08-30 02:13:06 +0900

    最新急上昇動画取得の時間を変更

--- src/laravel/app/Console/Kernel.php
+++ src/laravel/app/Console/Kernel.php
@@ -20,11 +20,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // 10:00、17:00に実行
+        // 毎日10:00、17:00に実行
         $schedule->command('youtube:get-trend-latest')
-            ->twiceDaily('10:00', '17:00');
+            ->twiceDaily(10, 17);
 
-        // 18:00に実行
+        // 毎日18:00に実行
         $schedule->command('youtube:get-trend-music')
             ->dailyAt('18:00');
     }
